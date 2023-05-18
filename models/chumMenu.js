d29c00159058c6155d6bfbebe4919a59044b2430
Dan <123746582+DanielFMcKay@users.noreply.github.com>
2023-05-18 05:35:03 -0700

    re-reverted some previous changes that were causing errors due to being undefined, defined Models in chumMenu.js since it was also undefined, changed ReadMe title to reflect current project

--- models/chumMenu.js
+++ models/chumMenu.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connections');
 // commented out because it is no longer being called in new format.
 // const User = require('./User');
