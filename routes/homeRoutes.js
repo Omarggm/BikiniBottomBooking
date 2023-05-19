89190f8a7888ac63410f2e37cfa7810d46e5877e
rslaughter12 <rslaughter12@gmail.com>
2023-05-18 20:54:07 -0700

    db updates

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -11,16 +11,12 @@ router.get('/', async (req, res) => {
 // loggedIn: req.session.loggedIn
 router.get('/krustykrab', async (req, res) => {
     try {
-<<<<<<< HEAD
-    res.render('krustykrab')} // before this render I need to fetch this information from the database
-=======
     const menuItems = [];
     res.render('krustykrab', {
         title: 'Krusty Krab',
         menuItems: menuItems,
     })
 }
->>>>>>> ef5479ca7860371cc1a9d97c775390be0b57a6a1
     catch (err) {
         res.status(500).json(err);
     }
