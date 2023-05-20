03e69b899948141fd41b7f3d724c570c848572d1
Walter Craigie <waltersaddress@gmail.com>
2023-05-20 19:57:30 -0400

    test 1

---
+++ routes/homeRoutes.js
@@ -0,0 +1,27 @@
+//template router.get taken from week14 mini-project
+
+// router.get('/', async (req, res) => {
+//     try {
+//       // Get all projects and JOIN with user data
+//       const projectData = await Project.findAll({
+//         include: [
+//           {
+//             model: User,
+//             attributes: ['name'],
+//           },
+//         ],
+//       });
+  
+//       // Serialize data so the template can read it
+//       const projects = projectData.map((project) => project.get({ plain: true }));
+  
+//       // Pass serialized data and session flag into template
+//       res.render('homepage', { 
+//         projects, 
+//         logged_in: req.session.logged_in 
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+  
