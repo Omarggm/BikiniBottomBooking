a92dc723d61cecf6e2c4e102932cc31d0aa3fdb7
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-05-21 21:10:05 -0700

    add post to usersRoutes.js tested in Postman

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -11,6 +11,16 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+    console.log(userData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
