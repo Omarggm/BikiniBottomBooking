54ac26df5f634a95b72c434e04530d97128cc743
Walter Craigie <waltersaddress@gmail.com>
2023-05-22 22:31:03 -0400

    added ohYeah howler to checkout buttons

--- public/js/howler.js
+++ public/js/howler.js
@@ -1,41 +1,49 @@
 // const Howler = require('howler');
 
 function sadSong() {
-    var sound = new Howl({
-      src: ["../sounds/sad-song.mp3"],
-      autoplay: true,
-      volume: 0.5,
-    });
-  }
-  
-  function booWomp() {
-    var sound = new Howl({
-      src: ["../sounds/boowomp.mp3"],
-      autoplay: true,
-      volume: 0.8,
-    });
-  }
-  
-  function stankNoise() {
-    var sound = new Howl({
-      src: ["../sounds/stank-noise.mp3"],
-      autoplay: true,
-      volume: 0.8,
-    });
-  }
-  
-  function garyMeow() {
-    var sound = new Howl({
-      src: ["../sounds/gary_meow.mp3"],
-      autoplay: true,
-      volume: 1.0,
-    });
-  }
-  
-  function squarePants() {
-    var sound = new Howl({
-      src: ["../sounds/squarepants.mp3"],
-      autoplay: true,
-      volume: 0.5,
-    });
+  var sound = new Howl({
+    src: ["../sounds/sad-song.mp3"],
+    autoplay: true,
+    volume: 0.5,
+  });
+}
+
+function booWomp() {
+  var sound = new Howl({
+    src: ["../sounds/boowomp.mp3"],
+    autoplay: true,
+    volume: 0.8,
+  });
+}
+
+function stankNoise() {
+  var sound = new Howl({
+    src: ["../sounds/stank-noise.mp3"],
+    autoplay: true,
+    volume: 0.8,
+  });
+}
+
+function garyMeow() {
+  var sound = new Howl({
+    src: ["../sounds/gary_meow.mp3"],
+    autoplay: true,
+    volume: 1.0,
+  });
+}
+
+function squarePants() {
+  var sound = new Howl({
+    src: ["../sounds/squarepants.mp3"],
+    autoplay: true,
+    volume: 0.5,
+  });
+}
+
+function ohYeah() {
+  var sound = new Howl({
+    src: ["../sounds/ohyeah.mp3"],
+    autoplay: true,
+    volume: 1.0,
+  });
 }
